3274b22a802b20c16d538012f4ff15b753195af7
VladislavYurchenko <59086101+VladislavYurchenko@users.noreply.github.com>
2023-02-08 15:49:58 +0300

    0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,99 @@
 /**
  * Webpack main configuration file
  */
+const pages = ["index", "page1", "page2"];
+const path = require("path");
+const fs = require("fs");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const path = require('path');
-const fs = require('fs');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const environment = require("./configuration/environment");
 
-const environment = require('./configuration/environment');
+// const templateFiles = fs
+//   .readdirSync(environment.paths.source)
+//   .filter((file) => [".html", ".ejs"].includes(path.extname(file).toLowerCase()))
+//   .map((filename) => ({
+//     input: filename,
+//     output: filename.replace(/\.ejs$/, ".html"),
+//   }));
 
-const templateFiles = fs.readdirSync(environment.paths.source)
-  .filter((file) => ['.html', '.ejs'].includes(path.extname(file).toLowerCase())).map((filename) => ({
-    input: filename,
-    output: filename.replace(/\.ejs$/, '.html'),
-  }));
-
-const htmlPluginEntries = templateFiles.map((template) => new HTMLWebpackPlugin({
-  inject: true,
-  hash: false,
-  filename: template.output,
-  template: path.resolve(environment.paths.source, template.input),
-  favicon: path.resolve(environment.paths.source, 'images', 'favicon.ico'),
-}));
+// const htmlPluginEntries = templateFiles.map(
+//   (template) =>
+//     new HTMLWebpackPlugin({
+//       inject: true,
+//       hash: false,
+//       filename: template.output,
+//       template: path.resolve(environment.paths.source, template.input),
+//       favicon: path.resolve(environment.paths.source, "images", "favicon.ico"),
+//     })
+// );
 
 module.exports = {
   entry: {
-    app: path.resolve(environment.paths.source, 'js', 'app.js'),
+    app: path.resolve(environment.paths.source, "js", "app.js"),
   },
+  // entry: pages.reduce((config, page) => {
+  //   config[page] = `./src/${page}.js`;
+  //   return config;
+  // }, {}),
   output: {
-    filename: 'js/[name].js',
+    filename: "js/[name].js",
     path: environment.paths.output,
   },
   module: {
     rules: [
       {
         test: /\.((c|sa|sc)ss)$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"],
       },
       {
         test: /\.(png|gif|jpe?g|svg)$/i,
-        type: 'asset',
+        type: "asset",
         parser: {
           dataUrlCondition: {
             maxSize: environment.limits.images,
           },
         },
         generator: {
-          filename: 'images/design/[name].[hash:6][ext]',
+          filename: "images/design/[name].[hash:6][ext]",
         },
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        type: 'asset',
+        type: "asset",
         parser: {
           dataUrlCondition: {
             maxSize: environment.limits.images,
           },
         },
         generator: {
-          filename: 'images/design/[name].[hash:6][ext]',
+          filename: "images/design/[name].[hash:6][ext]",
         },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              interpolate: true,
+            },
+          },
+        ],
       },
     ],
   },
   optimization: {
     minimizer: [
-      '...',
+      "...",
       new ImageMinimizerPlugin({
         minimizer: {
           implementation: ImageMinimizerPlugin.imageminMinify,
@@ -81,16 +101,16 @@ module.exports = {
             // Lossless optimization with custom option
             // Feel free to experiment with options for better result for you
             plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
               // Svgo configuration here https://github.com/svg/svgo#configuration
               [
-                'svgo',
+                "svgo",
                 {
                   plugins: [
                     {
-                      name: 'removeViewBox',
+                      name: "removeViewBox",
                       active: false,
                     },
                   ],
@@ -104,32 +124,44 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
+      filename: "css/[name].css",
     }),
     new CleanWebpackPlugin({
       verbose: true,
-      cleanOnceBeforeBuildPatterns: ['**/*', '!stats.json'],
+      cleanOnceBeforeBuildPatterns: ["**/*", "!stats.json"],
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(environment.paths.source, 'images', 'content'),
-          to: path.resolve(environment.paths.output, 'images', 'content'),
-          toType: 'dir',
+          from: path.resolve(environment.paths.source, "images", "content"),
+          to: path.resolve(environment.paths.output, "images", "content"),
+          toType: "dir",
           globOptions: {
-            ignore: ['*.DS_Store', 'Thumbs.db'],
+            ignore: ["*.DS_Store", "Thumbs.db"],
           },
         },
         {
-          from: path.resolve(environment.paths.source, 'videos'),
-          to: path.resolve(environment.paths.output, 'videos'),
-          toType: 'dir',
+          from: path.resolve(environment.paths.source, "videos"),
+          to: path.resolve(environment.paths.output, "videos"),
+          toType: "dir",
           globOptions: {
-            ignore: ['*.DS_Store', 'Thumbs.db'],
+            ignore: ["*.DS_Store", "Thumbs.db"],
           },
         },
       ],
     }),
-  ].concat(htmlPluginEntries),
-  target: 'web',
+  ]
+    // .concat(htmlPluginEntries),
+    .concat(
+      pages.map(
+        (page) =>
+          new HTMLWebpackPlugin({
+            inject: true,
+            template: path.resolve(environment.paths.source, `${page}.html`), // `./`,
+            filename: `${page}.html`,
+            chunks: [page],
+          })
+      )
+    ),
+  target: "web",
 };
